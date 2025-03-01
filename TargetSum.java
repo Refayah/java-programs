62db60919d2e7d274c91715b8550376b2648464d
Refayah <mv.refa@gmail.com>
2025-03-01 20:55:06 +0530

    sum of two elements in array equals the target

---
+++ TargetSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TargetSum {
+    public static void main(String args[])
+    {
+        int[] arr={-1,0,1,2,3,4,5,6,7,8};
+        int n=arr.length;
+        int target=7;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]+arr[j]==target)
+                {
+                    System.out.print(arr[i]+" "+arr[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
